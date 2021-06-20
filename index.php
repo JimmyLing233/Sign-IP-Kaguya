7e46925b9afdb33d96826b956363df7dff118a6a
宝安区浩哥 <33871744+JimmyLing233@users.noreply.github.com>
2021-06-20 21:59:23 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-header("Content-type: image/JPEG");
+header("Content-type: image/JPEG");//图片头
 include 'function.php';
 $im = imagecreatefromjpeg("xhxh.jpg"); 
 $ip = $_SERVER["REMOTE_ADDR"];//获取远程ip
@@ -17,16 +17,16 @@ $region = $data['site']['region']; //匹配region
 $black = ImageColorAllocate($im, 0,0,0);//定义黑色的值
 $red = ImageColorAllocate($im, 255,0,0);//红色
 $blue = ImageColorAllocate($im, 0,0,225);//蓝色
-$org = ImageColorAllocate($im, 255,69,0);//不知道是什么橘色
+$org = ImageColorAllocate($im, 255,69,0);//不知道是什么的橘色
 $green = ImageColorAllocate($im, 0,225,0);//绿色
 $violet = ImageColorAllocate($im, 168,0,211);//暗紫色
-$font = '/ali88Regular.ttf';//加载字体
+$font = '/ali88Regular.ttf';//加载字体(阿里巴巴公用字体)
 //输出
-imagettftext($im, 16, 0, 10, 40, $blue, $font,'来自'.$country.'-'.$region.'的朋友,欢迎您');
+imagettftext($im, 16, 0, 10, 40, $blue, $font,'来自'.$country.'-'.$region.'的朋友,欢迎您');//来自(国家)-(地区)的朋友,欢迎您
 imagettftext($im, 16, 0, 10, 72, $org, $font, '今天是'.date('Y年n月j日').' 星期'.$weekarray[date("w")]);//当前时间添加到图片
-imagettftext($im, 16, 0, 10, 104, $red, $font,'您的IP是:'.$ip);//ip
-imagettftext($im, 16, 0, 10, 140, $green, $font,'您使用的是'.$os.'操作系统');
-imagettftext($im, 16, 0, 10, 175, $violet, $font,'您使用的是'.$bro.'浏览器');
+imagettftext($im, 16, 0, 10, 104, $red, $font,'您的IP是:'.$ip);//ip地址
+imagettftext($im, 16, 0, 10, 140, $green, $font,'您使用的是'.$os.'操作系统');//显示操作系统
+imagettftext($im, 16, 0, 10, 175, $violet, $font,'您使用的是'.$bro.'浏览器');//显示当前浏览器
 ImageGif($im);
 ImageDestroy($im);
 ?>
